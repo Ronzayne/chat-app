30caa64f0775fb0f66884479020187c8e45f05f3
Ronzayne <ronzayne7@gmail.com>
2024-07-30 11:38:30 +0000

    1st-UI design completed

---
+++ src/providers/theme-provider.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import * as React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { type ThemeProviderProps } from "next-themes/dist/types";
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+}
+
+// this is for the dark and light mode which was taken from shadcn
